b18714fff09a9d17d8d7e1f829b544fa5b018a7b
EDUARDO FLORES <catocho61@gmail.com>
2025-09-06 02:06:21 -0600

    revision del index.html y app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
 // Array para almacenar los nombres de los amigos
 let amigos = [];
 
